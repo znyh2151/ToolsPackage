7557fb770d9b112ae3c58663e8005f4425448a93
ricardo <znyh2151@gmail.com>
2024-10-18 09:43:51 +0800

    修复读取版本好不准确问题

--- src/main/java/com/tools/bean/Config.kt
+++ src/main/java/com/tools/bean/Config.kt
@@ -3,9 +3,9 @@ package com.tools.bean
 
 const val CONFIG_NAME = "PackageToolsConfig.json"
 const val CONFIG_LOG = "PackageTools.log"
-const val VERSION_CODE_REGEX = "(?<=flavor[\\s\\S]{0,10}\\{[\\s\\S]{0,1000}?versionCode[\\s=]{0,10})\\d+(?=\\s*)"
-const val VERSION_NAME_REGEX = "(?<=flavor[\\s\\S]{0,10}\\{[\\s\\S]{0,1000}?versionName[\\s=]{0,10}\").*?(?=\")"
-const val APPLICATION_ID_REGEX = "(?<=flavor[\\s\\S]{0,10}\\{[\\s\\S]{0,1000}?applicationId[\\s=]{0,10}\").*?(?=\")"
+const val VERSION_CODE_REGEX = "(?<=flavor[ \\n]{0,10}\\{[\\s\\S]{0,1000}?versionCode[\\s=]{0,10})\\d+(?=\\s*)"
+const val VERSION_NAME_REGEX = "(?<=flavor[ \\n]{0,10}\\{[\\s\\S]{0,1000}?versionName[\\s=]{0,10}\\\").*?(?=\\\")"
+const val APPLICATION_ID_REGEX = "(?<=flavor[ \\n]{0,10}\\{[\\s\\S]{0,1000}?applicationId[\\s=]{0,10}\\\").*?(?=\\\")"
 const val FLAVORINFOS_REGEX = "(?<=flavorInfos=\").*?(?=\")"
 const val PLIST_URL_REGEX = "\\d+\\.\\d+\\.\\d+-\\d+(?=.la)"
 
